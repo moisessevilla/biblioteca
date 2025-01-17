25bfafa2d4c582b61f253a04cb633cee4d0044c7
moisessevilla <163861638+moisessevilla@users.noreply.github.com>
2025-01-17 17:49:17 +0100

    Delete 21GIIN - Actividad 3 directory

--- 21GIIN - Actividad 3/verificarPrestamo.js
+++
@@ -1,7 +0,0 @@
-async function puedePrestar(usuarioId) {
-    const query = 'SELECT COUNT(*) AS count FROM prestamos WHERE usuario_id = ? AND devuelto = 0';
-    const [rows] = await db.promise().query(query, [usuarioId]);
-    const maxPrestamos = 2; // Define el máximo de libros permitidos
-    return rows[0].count < maxPrestamos; // Retorna true si no excede el límite
-  }
-  
