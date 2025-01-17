4860f28d3092219973c312a6e0957dbe29f8fcf4
moisessevilla <163861638+moisessevilla@users.noreply.github.com>
2025-01-17 17:32:45 +0100

    Delete registrarPrestamo.js

--- registrarPrestamo.js
+++
@@ -1,9 +0,0 @@
-async function registrarPrestamo(usuarioId, libroId) {
-    if (!(await puedePrestar(usuarioId))) {
-      throw new Error('El usuario ya alcanzó el límite máximo de libros prestados');
-    }
-  
-    const query = 'INSERT INTO prestamos (usuario_id, libro_id, fecha_prestamo, devuelto) VALUES (?, ?, NOW(), 0)';
-    await db.promise().query(query, [usuarioId, libroId]);
-  }
-  
